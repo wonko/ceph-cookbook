4f7d18f92837c2a9bcb0d7548a7ad5d8510e45ca
Bernard Grymonpon <bernard@openminds.be>
2011-12-29 14:24:20 +0100

    ceph client

---
+++ recipes/client.rb
@@ -0,0 +1,14 @@
+# we use the official ceph repo
+include_recipe "apt"
+
+apt_repository "ceph" do
+  uri "http://ceph.newdream.net/debian/"
+  distribution node['lsb']['codename'] 
+  components ["main"]
+  key "https://raw.github.com/NewDreamNetwork/ceph/master/keys/release.asc"
+  action :add
+end
+
+%w{ceph-common}.each do |name|
+  package name
+end
